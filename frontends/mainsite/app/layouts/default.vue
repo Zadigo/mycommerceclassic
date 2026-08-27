43ee53b8dea8099302371ab1efda1a3b469341bf
Zadigo <pendenquejohn@gmail.com>
2026-08-27 16:41:46 +0200

    Ecommerce menu update

--- frontends/mainsite/app/layouts/default.vue
+++ frontends/mainsite/app/layouts/default.vue
@@ -2,7 +2,7 @@
   <section id="site">
     <header class="fixed top-0 left-0 w-full z-50">
       <base-navbar />
-      <lazy-base-ecommerce-menu />
+      <!-- <lazy-base-ecommerce-menu /> -->
     </header>
 
     <main class="pt-[calc(65px)]">
