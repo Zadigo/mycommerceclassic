1071f3d023806ee9009d960a09cf0bba365fb4fe
Zadigo <pendenquejohn@gmail.com>
2026-08-28 18:43:31 +0200

    Animate checkboxes

--- frontends/mainsite/app/components/products/filters/Selected.vue
+++ frontends/mainsite/app/components/products/filters/Selected.vue
@@ -1,6 +1,9 @@
 <template>
   <div v-show="selection.length > 0" class="flex justify-start flex-wrap gap-1 mb-5">
-    <u-badge v-for="item in selection" :key="item" :label="item" size="lg" variant="subtle" color="info" icon="i-lucide-x" @click="emit('remove-filter', item)" />
+    <motion-group :preset="VueUseMotions.Fade">
+      <u-badge v-for="item in selection" :key="item" :label="item" size="lg" variant="subtle" color="info" icon="i-lucide-x" @click="emit('remove-filter', item)" />
+    </motion-group>
+    
     <u-button variant="subtle" color="info" @click="emit('remove-filters')">
       <icon name="i-lucide-trash-2" />
     </u-button>
